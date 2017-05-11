ce0b05304030b0d52686528397697756e46be9c0
Péter Biró <biro.peter.hu@gmail.com>
2017-05-11 15:20:08 +0200

    edit anser added to this marvelous app

--- main.py
+++ main.py
@@ -81,6 +81,26 @@ def displays_a_single_question_A(question_id):
     return render_template("display_a_question.html", question_with_answers=question_with_answers)
 
 
+@app.route("/answer/<answer_id>/edit")
+def edit_answer(answer_id):
+    answer = common.get_answer(answer_id)
+    question = common.get_question(answer["question_id"])
+    return render_template("answer.html", question=question, answer=answer, mode="Edit", error="")
+
+
+@app.route("/answer/<answer_id>/edit", methods=["POST"])
+def edit_answer_post(answer_id):
+    if len(request.form["answer"]) < 10:
+        answer = common.get_answer(answer_id)
+        question = common.get_question(answer["question_id"])
+        return render_template("answer.html", question=question, answer=answer, mode="Edit", error="10 char")
+    answers_list = data_manager.get_dict("answer", "answer.csv")
+    answers_list[common.get_index_from_id(answers_list, answer_id)]["message"] = request.form["answer"]
+    data_manager.save_dict(answers_list, "answer", "answer.csv")
+    question_id = answers_list[common.get_index_from_id(answers_list, answer_id)]["question_id"]
+    return redirect("/question/{}".format(question_id), code=302)
+
+
 @app.route("/answer/<answer_id>/delete")
 def delete_answer(answer_id):
     answer = common.get_answer(answer_id)
@@ -100,14 +120,14 @@ def delete_answer_post(answer_id):
 @app.route("/question/<question_id>/new-answer")
 def new_answer(question_id):
     question = common.get_question(question_id)
-    return render_template("answer.html", question=question, error="")
+    return render_template("answer.html", question=question, mode="Send new", error="")
 
 
 @app.route("/question/<question_id>/new-answer", methods=["POST"])
 def new_answer_post(question_id):
     if len(request.form["answer"]) < 10:
         question = common.get_question(question_id)
-        return render_template("answer.html", question=question, error="10 char")
+        return render_template("answer.html", question=question, mode="Send new", error="")
     answers_list = data_manager.get_dict("answer", "answer.csv")
     answer = {}
     answer["answer_id"] = str(common.get_max_id(answers_list) + 1)
@@ -118,7 +138,7 @@ def new_answer_post(question_id):
     answer["image"] = ""
     answers_list.append(answer)
     data_manager.save_dict(answers_list, "answer", "answer.csv")
-    return redirect("/", code=302)
+    return redirect("/", code=302))
 
 
 @app.route("/question/<int:question_id>/answer/<int:answer_id>/edit")
