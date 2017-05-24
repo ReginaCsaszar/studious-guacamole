1437ffdcd5e6a61671e003e47d14f51ded72a065
Péter Biró <biro.peter.hu@gmail.com>
2017-05-24 13:35:19 +0200

    build_dict() implemented

--- data_manager.py
+++ data_manager.py
@@ -3,6 +3,15 @@ from private_psql_settings import connectioon_triplets
 
 
 def run_query(query):
+    """
+    A single query from the localhost database.
+    Arg:
+        @query: string - a whole executable psql query
+    return:
+        - for SELECT: list of tuples
+        - in case of error: error message
+        - "Done :)" if non SELECT query executed without error
+    """
     try:
         dbname, user, password = connectioon_triplets()
         connect_str = "dbname='{}' user='{}' host='localhost' password='{}'".format(dbname, user, password)
@@ -29,9 +38,27 @@ def run_query(query):
     return rows
 
 
+def build_dict(table, key_words):
+    """
+    Build a list of dictionaries with given keys.
+    Args:
+        @table: list of tuples (or lists)
+        @key_words: list of strings in the order of the element of tuples
+    Return:
+        list of dictionaries
+    """
+    result = []
+    for row in table:
+        record = {}
+        for key, value in zip(key_words, row):
+            record[key] = value
+        result.append(record)
+    return result
+
+
 def main():
-    result = run_query("SELECT * FROM question;")
-    print(result)
+   pass
+
 
 if __name__ == '__main__':
     main()
