2df71b66e93a23c606861a979cf278845a215549
hankoattila <hattilla4@gmail.com>
2017-05-24 21:37:17 +0200

    question_id and answer_id key is changed to id, the vote part is waiting for upgrade

--- displays_a_question.py
+++ displays_a_question.py
@@ -24,40 +24,38 @@ def vote_answer_down(question_id, answer_id):
     return redirect("/question/{0}".format(question_id))
 
 
-def vote(type_, filename, direction, id, key):
-    query = "SELECT * FROM question WHERE id= '%'" % (id)
+def vote(type_, list_of_key, direction, id):
+    query = "SELECT * FROM {0} WHERE id={1}".format(type_, id)
     data = data_manager.run_query(query)
-    for row in data:
-        if int(row[key]) == id:
-            if direction == "up":
-                row["vote_number"] = str(int(row["vote_number"]) + 1)
-            else:
-                row["vote_number"] = str(int(row["vote_number"]) - 1)
-    data_manager.save_dict(data, type_, filename)
+    type_ = data_manager.build_dict(data, list_of_key)
+    if direction == 'up':
+        type_[0]["vote_number"] += 1
+    else:
+        type_[0]["vote_number"] -= 1
 
 
 def displays_a_single_question(question_id):
 
-    list_of_key_of_question = ["question_id", "submisson_time", "view_number", "vote_number", "title", "message"]
+    list_of_key_of_question = ["id", "submisson_time", "view_number", "vote_number", "title", "message"]
     title_of_question = ["ID", "Submisson time", "View number", "Vote number", "Title", "Message"]
     list_of_key_and_title_of_question = list(zip(list_of_key_of_question, title_of_question))
 
-    list_of_key_of_answer = ["answer_id", "submisson_time", "vote_number", "question_id", "message"]
+    list_of_key_of_answer = ["id", "submisson_time", "vote_number", "question_id", "message"]
     title_of_answer = ["ID", "Submisson time", "Vote number", "Question id", "Message"]
     list_of_key_and_title_of_answers = list(zip(list_of_key_of_answer, title_of_answer))
 
     query = "SELECT * FROM question"
     rows = data_manager.run_query(query)
-    list_of_names = ["question_id", "submisson_time", "view_number", "vote_number", "title", "message"]
+    list_of_names = ["id", "submisson_time", "view_number", "vote_number", "title", "message"]
     all_question = data_manager.build_dict(rows, list_of_names)
     for question_ in all_question:
-        if question_id == question_["question_id"]:
+        if question_id == question_["id"]:
             question = question_
             break
 
     query = "SELECT * FROM answer"
     rows = data_manager.run_query(query)
-    list_of_names = ["answer_id", "submisson_time", "vote_number", "question_id", "message", "image"]
+    list_of_names = ["id", "submisson_time", "vote_number", "question_id", "message"]
     all_answers = data_manager.build_dict(rows, list_of_names)
     answers = []
     for answer in all_answers:
@@ -85,8 +83,12 @@ def sort(data, sort_by, direction):
 
     return sorted_data
 
+
 if __name__ == "__main__":
     app.run(debug=True)
+    """
+    list_of_key = ["question_id", "submisson_time", "view_number", "vote_number", "title", "message"]
+    print(vote('answer', list_of_key, 'down', 1))"""
 
 
 
