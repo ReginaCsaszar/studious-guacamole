90d1edf7d649c82410c3da1c6bde22b5c0537020
Péter Biró <biro.peter.hu@gmail.com>
2017-05-24 12:28:02 +0200

    run_query implemented

--- data_manager.py
+++ data_manager.py
@@ -2,12 +2,37 @@ import psycopg2
 from private_psql_settings import connectioon_triplets
 
 
-def connect_to_db():
-    dbname, user, password = connectioon_triplets()
+def run_query(query):
+    try:
+        dbname, user, password = connectioon_triplets()
+        connect_str = "dbname='{}' user='{}' host='localhost' password='{}'".format(dbname, user, password)
+        connection = psycopg2.connect(connect_str)
+        cursor = connection.cursor()
+        connection.autocommit = True
+        cursor = connection.cursor()
+
+        cursor.execute(query)
+
+        if query.upper().startswith("SELECT"):
+            rows = cursor.fetchall()
+        else:
+            rows = "Done :)"
+        cursor.close()
+
+    except psycopg2.DatabaseError as exception:
+        print(exception)
+        rows = exception
+
+    finally:
+        if connection:
+            connection.close()
+    return rows
 
 
 def main():
-    pass
+    result = run_query("SELECT * FROM question;")
+    print(result)
 
 if __name__ == '__main__':
     main()
+
