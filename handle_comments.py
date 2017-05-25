30bdbfb081a52d0fbb02fd6b3db6e91485c1a816
gergelyHorvath <gergely.horvath126@gmail.com>
2017-05-25 15:35:59 +0200

    typo correction in main.py

--- handle_comments.py
+++ handle_comments.py
@@ -1,22 +1,27 @@
 import datetime
 import data_manager
-from flask import FLASK, render_template
+from flask import Flask, render_template
 
 
 def add_new_question_comment(question_id):
     sql_query = """SELECT message FROM question WHERE id='question_id'"""
     question_content = data_manager.run_query(sql_query)[0][0]
     return render_template(
-        'comment.html', basis='question', mode='Add_new', button='Create'
-        content=question_content, question_id='question_id', answer_id='', title='Add a'
+        'comment.html', basis='question', mode='Add_new', button='Create',
+        content=question_content, question_id=question_id, answer_id='', title='Add a'
         )
 
 
 def add_comment_to_db(question_id, answer_id, comment):
+    if not answer_id:
+        q_id = question_id
+        answer_id = 'IS NULL'
+    else:
+        q_id = 'IS NULL'
     curr_time = str(datetime.datetime.now())[:16]
     sql_query = (
         """INSERT INTO comment (question_id, answer_id, message, submission_time, edited_number)
-        VALUES ({}, is NULL, {}, {}, 0 )""".format(question_id, comment, curr_time)
+        VALUES ({}, {}, {}, {}, 0 )""".format(q_id, answer_id, comment, curr_time)
     )
     data_manager.run_query(sql_query)
     return redirect('/question/' + question_id)
