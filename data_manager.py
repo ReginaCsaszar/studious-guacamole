1a04bcd92e8b06903e3597459ac418b4dcb0f4b1
Péter Biró <biro.peter.hu@gmail.com>
2017-05-25 15:07:59 +0200

    safe_insert added

--- data_manager.py
+++ data_manager.py
@@ -2,6 +2,35 @@ import psycopg2
 from private_psql_settings import connectioon_triplets
 
 
+def safe_insert(table, column_list, value_list):
+    """
+    Almost safe INSERT INTO builder :)
+    Args:
+        @table: string - name of the table
+        @column_list: list of strings
+        @value_list: list of values to be inserted str/int/anything (hopefully)
+    """
+    try:
+        dbname, user, password = connectioon_triplets()
+        connect_str = "dbname='{}' user='{}' host='localhost' password='{}'".format(dbname, user, password)
+        connection = psycopg2.connect(connect_str)
+        cursor = connection.cursor()
+        connection.autocommit = True
+        cursor = connection.cursor()
+
+        placeholder = ", ".join(["%%s" for _ in value_list])
+        query = "INSERT INTO %s ({0}) VALUES ({1})".format(", ".join(column_list), placeholder)
+        cursor.execute(query % table, value_list)
+
+        cursor.close()
+    except psycopg2.DatabaseError as exception:
+        print(exception)
+    finally:
+        if connection:
+            connection.close()
+    return
+
+
 def run_query(query):
     """
     A single query from the localhost database.
@@ -57,7 +86,7 @@ def build_dict(table, key_words):
 
 
 def main():
-    pass
+    safe_insert("answer", ["message", "question_id"], ["kasdjhksdfg", "1"])
 
 if __name__ == '__main__':
     main()
