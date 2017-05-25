a40c85e7461dec68829a9a36f078eb72f0249903
gergelyHorvath <gergely.horvath126@gmail.com>
2017-05-25 17:21:13 +0200

    added working version of  new  comment to question feature , in handle_commit.py, commit.html

--- handle_comments.py
+++ handle_comments.py
@@ -1,28 +1,32 @@
 import datetime
 import data_manager
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 
 def add_new_question_comment(question_id):
-    sql_query = """SELECT message FROM question WHERE id='question_id'"""
+    sql_query = """SELECT message FROM question WHERE id={}""".format(question_id)
     question_content = data_manager.run_query(sql_query)[0][0]
     return render_template(
         'comment.html', basis='question', mode='Add_new', button='Create',
-        content=question_content, question_id=question_id, answer_id='', title='Add a'
+        content=question_content, question_id=str(question_id), answer_id='', title='Add a'
         )
 
 
-def add_comment_to_db(question_id, answer_id, comment):
-    if not answer_id:
-        q_id = question_id
-        answer_id = 'IS NULL'
-    else:
-        q_id = 'IS NULL'
+def add_comment_to_db(q_or_a, id, comment):
     curr_time = str(datetime.datetime.now())[:16]
-    sql_query = (
-        """INSERT INTO comment (question_id, answer_id, message, submission_time, edited_number)
-        VALUES ({}, {}, {}, {}, 0 )""".format(q_id, answer_id, comment, curr_time)
-    )
+    if q_or_a == 'question':
+        print(id, comment, curr_time)
+        sql_query = (
+            """INSERT INTO comment (question_id, message, submission_time)
+            VALUES ({}, '{}', '{}');""".format(id, comment, curr_time)
+        )
+        question_id = id
+    else:
+        sql_query = (
+            """INSERT INTO comment (answer_id, message, submission_time, edited_number)
+            VALUES ({}, {}, {}, 0 )""".format(id, comment, curr_time)
+        )
+        question_id = data_manager.run_query("""SELECT question_id FROM answer WHERE id={}""".format(id))
     data_manager.run_query(sql_query)
     return redirect('/question/' + question_id)
 
