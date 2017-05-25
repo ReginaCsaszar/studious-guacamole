8e3fc4a42fdf43d9626f495bca4d9d88b2d4e004
ReginaCsaszar <regina.csaszar@gmail.com>
2017-05-25 14:17:09 +0200

    cleansing the code in search.py

--- search.py
+++ search.py
@@ -1,17 +1,10 @@
-"""As a user I want to search in questions & answers.
-There should be a search box on the main page and a button.
-If I write something in the search box and press the button:
-I want to see a list of all questions (same data as in the list page):
- - which title contains the text in the search box or
- - which have answers which message contains the text in the search box.
-(/search?q=<search phrase>
+"""Search in questions & answers module
 """
 
 from flask import Flask, request, render_template, redirect
 import data_manager
 
 
-# @app.route('/search?q=<search phrase>')
 def search_questions(term):
     answer_query = """SELECT question_id FROM answer
                    WHERE message LIKE '%{0}%' ;""".format(term)
@@ -27,8 +20,8 @@ def search_questions(term):
 
 
 def main():
-    print(search_questions("e"))
     pass
+
 
 if __name__ == '__main__':
     main()
