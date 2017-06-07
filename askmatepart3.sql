f508851cd1c5b1be2303c4f9f575a748f81c4361
ReginaCsaszar <regina.csaszar@gmail.com>
2017-06-07 18:35:00 +0200

    add an option field for selecting user for comments, modify main.py and content.py to handle users

--- askmatepart3.sql
+++ askmatepart3.sql
@@ -155,8 +155,8 @@ INSERT INTO answer VALUES (1, '2017-04-28 16:49:00', 4, 0, 'You need to use brac
 INSERT INTO answer VALUES (2, '2017-04-25 14:42:00', 35, 0, 'Look it up in the Python docs', 'images/image2.jpg', FALSE, 3);
 SELECT pg_catalog.setval('answer_id_seq', 2, true);
 
-INSERT INTO comment VALUES (1, 0, NULL, 'Please clarify the question as it is too vague!', '2017-05-01 05:49:00', 1);
-INSERT INTO comment VALUES (2, NULL, 1, 'I think you could use my_list = list() as well.', '2017-05-02 16:55:00', 3);
+INSERT INTO comment VALUES (1, 0, NULL, 'Please clarify the question as it is too vague!', '2017-05-01 05:49:00', 1, 2);
+INSERT INTO comment VALUES (2, NULL, 1, 'I think you could use my_list = list() as well.', '2017-05-02 16:55:00', 3, 2);
 SELECT pg_catalog.setval('comment_id_seq', 2, true);
 
 INSERT INTO tag VALUES (1, 'python');
