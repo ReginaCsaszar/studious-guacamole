72c75a803c34ea66ddc431445fea46b67fa482ab
ReginaCsaszar <regina.csaszar@gmail.com>
2017-06-08 17:40:04 +0200

    add more sample data

--- askmatepart3.sql
+++ askmatepart3.sql
@@ -20,7 +20,7 @@ DROP TABLE IF EXISTS public.question;
 DROP SEQUENCE IF EXISTS public.question_id_seq;
 CREATE TABLE question (
     id serial NOT NULL,
-    submission_time char(16) default to_char(LOCALTIMESTAMP, 'YYYY-MM-DD HH24:MI')
+    submission_time char(16) default to_char(LOCALTIMESTAMP, 'YYYY-MM-DD HH24:MI'),
     view_number integer DEFAULT 0, 
     vote_number integer DEFAULT 0,
     title text,
@@ -33,7 +33,7 @@ DROP TABLE IF EXISTS public.answer;
 DROP SEQUENCE IF EXISTS public.answer_id_seq;
 CREATE TABLE answer (
     id serial NOT NULL,
-    submission_time char(16) default to_char(LOCALTIMESTAMP, 'YYYY-MM-DD HH24:MI')
+    submission_time char(16) default to_char(LOCALTIMESTAMP, 'YYYY-MM-DD HH24:MI'),
     vote_number integer DEFAULT 0,
     question_id integer,
     message text,
@@ -49,7 +49,7 @@ CREATE TABLE comment (
     question_id integer,
     answer_id integer,
     message text,
-    submission_time char(16) default to_char(LOCALTIMESTAMP, 'YYYY-MM-DD HH24:MI')
+    submission_time char(16) default to_char(LOCALTIMESTAMP, 'YYYY-MM-DD HH24:MI'),
     edited_count integer DEFAULT 0,
     users_id integer
 );
@@ -131,10 +131,13 @@ ALTER TABLE ONLY question_tag
     ON DELETE CASCADE;
 
 
-INSERT INTO users (name) VALUES ('Jane');
-INSERT INTO users (name) VALUES ('John');
-INSERT INTO users (name) VALUES ('Bruce');
-SELECT pg_catalog.setval('users_id_seq', 3, true);
+INSERT INTO users (name) VALUES ('Jeannie');
+INSERT INTO users (name) VALUES ('Greg');
+INSERT INTO users (name) VALUES ('Peter');
+INSERT INTO users (name) VALUES ('Attila');
+INSERT INTO users (name) VALUES ('Helga');
+INSERT INTO users (name) VALUES ('Barna');
+SELECT pg_catalog.setval('users_id_seq', 6, true);
 
 
 INSERT INTO question VALUES (0, '2017-04-28 08:29', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', NULL, 2);
@@ -175,12 +178,22 @@ The system tables live in the pg_catalog database.', NULL, 3);
 SELECT pg_catalog.setval('question_id_seq', 4, true);
 
 INSERT INTO answer VALUES (1, '2017-04-28 16:49', 4, 0, 'You need to use brackets: my_list = []', NULL, FALSE, 1);
-INSERT INTO answer VALUES (2, '2017-04-25 14:42', 35, 0, 'Look it up in the Python docs', 'images/image2.jpg', FALSE, 3);
-SELECT pg_catalog.setval('answer_id_seq', 2, true);
+INSERT INTO answer VALUES (2, '2017-04-27 14:42', 35, 0, 'Look it up in the Python docs', 'images/image2.jpg', FALSE, 3);
+INSERT INTO answer VALUES (3, '2017-04-21 12:25', 0, 0, 'Notice how I am creating an empty array (list500steps) each time in the first for loop? Then, after creating all the steps I append that array (Which is now NOT empty) to the array of walks.', NULL, FALSE, 2);
+INSERT INTO answer VALUES (4, '2017-04-25 19:05', 0, 1, 'Do you really need to use multiple versions of jQuery? Doing that normally creates more problems than it solves', NULL, FALSE, 2);
+INSERT INTO answer VALUES (5, '2017-04-24 16:21', 0, 1, 'no, no, no exactly BECAUSE i have multiple versions (wordpress loads jquery and my theme loads jquery) it causes problems', NULL, FALSE, 2);
+INSERT INTO answer VALUES (6, '2017-04-23 08:56', 0, 2, 'There is no difference, under the hood it''s all varlena', NULL, FALSE, 2);
+INSERT INTO answer VALUES (7, '2017-04-26 02:48', 0, 3, 'varchar(5) is better, note however that you''ve already limited the length of string using check. On a side note I''m using solely text. In the quoted post you can find some benchmarks as well.', NULL, FALSE, 2);
+INSERT INTO answer VALUES (8, '2017-04-22 10:19', 0, 4, 'Login as superuser: sudo -u postgres psql. You can list all databases and users by \l command, (list other commands by \?). Now if you want to see other databases you can change user/database by \c command like \c template1, \c postgres postgres and use \d, \dt or \dS to see tables/views/etc.', NULL, FALSE, 2);
+SELECT pg_catalog.setval('answer_id_seq', 8, true);
 
 INSERT INTO comment VALUES (1, 0, NULL, 'Please clarify the question as it is too vague!', '2017-05-01 05:49', 1, 2);
 INSERT INTO comment VALUES (2, NULL, 1, 'I think you could use my_list = list() as well.', '2017-05-02 16:55', 3, 2);
-SELECT pg_catalog.setval('comment_id_seq', 2, true);
+INSERT INTO comment VALUES (3, 2, NULL, 'That makes sense. Thanks a lot', '2017-05-21 08:24', 1, 2);
+INSERT INTO comment VALUES (4, NULL, 8, 'Can you use the in operator?', '2017-05-24 12:15', 3, 2);
+INSERT INTO comment VALUES (5, NULL, 2, 'Thanks, Captain Obvious', '2017-05-26 13:15', 3, 2);
+INSERT INTO comment VALUES (6, NULL, 3, 'Yep you are right. I forgot empty lists.', '2017-05-26 15:14', 3, 2);
+SELECT pg_catalog.setval('comment_id_seq', 4, true);
 
 INSERT INTO tag VALUES (1, 'python', 'rgb(0, 128, 255)');
 INSERT INTO tag VALUES (2, 'sql', 'rgb(0, 255, 64)');
