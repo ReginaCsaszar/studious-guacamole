9e3090285e305ce74aa2f60a7c04ffea2f55fff4
ReginaCsaszar <regina.csaszar@gmail.com>
2017-06-09 09:57:22 +0200

    restore comment changes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import listpage
 import random
 import handle_question
 import handle_comments
+import handle_users
 import search
 from flask import Flask, request, render_template, redirect
 from displays_a_question import question_up
@@ -187,7 +188,8 @@ def add_new_answer_comment(answer_id):
 @app.route('/add_comment_to_db/<q_or_a>/<id>')
 def add_comment_to_db(q_or_a, id):
     commit = request.args['comment']
-    return handle_comments.add_comment_to_db(q_or_a, id, commit)
+    name = request.args['names']
+    return handle_comments.add_comment_to_db(q_or_a, id, commit, name)
 
 
 @app.route('/comments/<comment_id>/edit')
